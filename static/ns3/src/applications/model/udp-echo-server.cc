14075e5fd2d714b91164ed8c71542862065ca663
Samir SI-MOHAMMED <samir.si-mohammed@inria.fr>
2022-02-22 18:36:35 +0100

    Removed latency from logs

--- static/ns3/src/applications/model/udp-echo-server.cc
+++ static/ns3/src/applications/model/udp-echo-server.cc
@@ -170,12 +170,18 @@
            NS_LOG_INFO ("At time " << Simulator::Now ().As (Time::S) << " server received " << packet->GetSize () << " bytes from " <<
                         InetSocketAddress::ConvertFrom (from).GetIpv4 () << " port " <<
                         InetSocketAddress::ConvertFrom (from).GetPort ());
+          std::cout << "At time " << Simulator::Now ().As (Time::S) << " server received " << packet->GetSize () << " bytes from " <<
+                        InetSocketAddress::ConvertFrom (from).GetIpv4 () << " port " <<
+                        InetSocketAddress::ConvertFrom (from).GetPort () << std::endl;
          }
        else if (Inet6SocketAddress::IsMatchingType (from))
          {
            NS_LOG_INFO ("At time " << Simulator::Now ().As (Time::S) << " server received " << packet->GetSize () << " bytes from " <<
                         Inet6SocketAddress::ConvertFrom (from).GetIpv6 () << " port " <<
                         Inet6SocketAddress::ConvertFrom (from).GetPort ());
+          std::cout << "At time " << Simulator::Now ().As (Time::S) << " server received " << packet->GetSize () << " bytes from " <<
+                        Inet6SocketAddress::ConvertFrom (from).GetIpv6 () << " port " <<
+                        Inet6SocketAddress::ConvertFrom (from).GetPort () << std::endl;
          }
   
        packet->RemoveAllPacketTags ();
@@ -189,12 +195,18 @@
            NS_LOG_INFO ("At time " << Simulator::Now ().As (Time::S) << " server sent " << packet->GetSize () << " bytes to " <<
                         InetSocketAddress::ConvertFrom (from).GetIpv4 () << " port " <<
                         InetSocketAddress::ConvertFrom (from).GetPort ());
+          std::cout << "At time " << Simulator::Now ().As (Time::S) << " server sent " << packet->GetSize () << " bytes to " <<
+                        InetSocketAddress::ConvertFrom (from).GetIpv4 () << " port " <<
+                        InetSocketAddress::ConvertFrom (from).GetPort () << std::endl;
          }
        else if (Inet6SocketAddress::IsMatchingType (from))
          {
            NS_LOG_INFO ("At time " << Simulator::Now ().As (Time::S) << " server sent " << packet->GetSize () << " bytes to " <<
                         Inet6SocketAddress::ConvertFrom (from).GetIpv6 () << " port " <<
                         Inet6SocketAddress::ConvertFrom (from).GetPort ());
+          std::cout << "At time " << Simulator::Now ().As (Time::S) << " server sent " << packet->GetSize () << " bytes to " <<
+                        Inet6SocketAddress::ConvertFrom (from).GetIpv6 () << " port " <<
+                        Inet6SocketAddress::ConvertFrom (from).GetPort () << std::endl;
          }
      }
  }
