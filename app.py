42d061490d110da7068f28fbb94446ef3b2c3764
SamirSim <samirsim98@gmail.com>
2022-06-15 14:03:12 +0200

    Test ./waf run

--- app.py
+++ app.py
@@ -271,7 +271,7 @@ def index():
                         elif os.environ['TRAFFICPROF'] == "periodic":
                             if os.environ['NETWORK'] == "Wi-Fi 802.11ac":
                                 #output = _check_output(cd_ns3_dir +'./waf --jobs=2 --run "wifi-periodic --distance=$DISTANCE --simulationTime=$SIMULATION_TIME --nWifi=$NUMDEVICES --trafficDirection=$TRAFFICDIR --payloadSize=$PACKETSIZE --period=$LOADFREQ --hiddenStations=$HIDDENDEVICES --radioEnvironment=$RADIOENVIRONMENT --spatialStreams=$SPATIALSTREAMS --txCurrent=$TXCURRENT --rxCurrent=$RXCURRENT --idleCurrent=$IDLECURRENT --ccaBusyCurrent=$CCABUSYCURRENT --MCS=$MCS --channelWidth=$BANDWIDTH --propDelay=$PROPDELAY --spatialStreams=$SPATIALSTREAMS --batteryCap=$BATTERYCAP --voltage=$VOLTAGE 2> log.txt"')
-                                output = _check_output(cd_ns3_dir +'./waf --jobs=2 --run wifi-periodic')
+                                output = _check_output(cd_ns3_dir +'./waf')
                                 print("OUTPUT: ", output)
                                 with open("log.txt", "w") as text_file:
                                     text_file.write(output)
